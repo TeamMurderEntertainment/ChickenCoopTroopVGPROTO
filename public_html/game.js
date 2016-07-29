910b33ad111d6d758ec7e151521dfc43af67e273
Epaggelia <Holmes12345@outlook.com>
2016-07-28 22:48:17 -0400

    probably did something?

--- public_html/game.js
+++ public_html/game.js
@@ -116,7 +116,7 @@ function startLogic()
 						);
 			}
 		}
-		genMap(); ////////////////////////////////////////////////////////////////
+		redraw(); ////////////////////////////////////////////////////////////////
 	}
 
 	function showMenu()
@@ -158,6 +158,12 @@ function startLogic()
 	function genMap()
 	{
 		var spriteNum = 0;
+		var EDGE = 2;		//defaults to fenceless edge
+		var CORNER = 3;		//defaults to fenceless corner
+		
+		if (Math.random() > 0.5) //random fence
+			EDGE = 4;
+			CORNER = 5;
 		
 		for ( var x = 0; x < tileCountX; x++ )
 		{
@@ -166,16 +172,40 @@ function startLogic()
 			{
 				spriteTiles[x][y] = 0;
 				
-				if (x == 0)
-					spriteTiles[x][y] = 2;
-				if (x == tileCountX-1)
-					spriteTiles[x][y] = 2;
-				if (y == 0)
-					spriteTiles[x][y] = 2;
-				if (y == tileCountY-1)
-					spriteTiles[x][y] = 2;
+				//level border in clockwise order from top left
+				if (y == 0)								// top side
+					spriteTiles[x][y] = EDGE;
+				if (x == tileCountX-1)					// right side
+					spriteTiles[x][y] = EDGE;
+				if (y == tileCountY-1)					// bottom side
+					spriteTiles[x][y] = EDGE;
+				if (x == 0)								// left side
+					spriteTiles[x][y] = EDGE;
 				
+				//level corner --- draws over border
+				if (x == 0 && y == 0)						//NW
+					spriteTiles[x][y] = CORNER;
+				if (x == tileCountX-1 && y == 0)			//NE
+					spriteTiles[x][y] = CORNER;
+				if (x == tileCountX-1 && y == tileCountY-1)	//SE
+					spriteTiles[x][y] = CORNER;
+				if (x == 0 && y == tileCountY-1)			//SW
+					spriteTiles[x][y] = CORNER;
 				
+				//random rocks 1 space inside border
+				if (x > 1 && x < (tileCountX-1)-1)
+					if (y > 1 && y < (tileCountY-1)-1)
+						if (Math.random() > 0.95)
+						spriteTiles[x][y] = 1;
+			}
+		}
+	}
+	
+	function redraw() {
+		for (var x = 0; x < tileCountX; x++)
+		{
+			for (var y = 0; y < tileCountY; y++)
+			{
 				switch (spriteTiles[x][y])
 				{
 					case 5: //fenceCorner
@@ -197,16 +227,17 @@ function startLogic()
 						spriteNum = 0;
 						break;
 				}
-				
+
 				ctx.drawImage(image,
-					spriteNum * 32,			// srcX
-					0,						// srcY
-					32, 32,
-					x * bgTile.w, y * bgTile.h,
-					32, 32);
+						Math.floor(spriteNum) * 32, // srcX
+						0, // srcY
+						32, 32,
+						x * bgTile.w, y * bgTile.h,
+						32, 32);
 			}
 		}
 	}
-
+	
+	genMap();
 	update();
 }
