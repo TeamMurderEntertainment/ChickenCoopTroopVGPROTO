95365cd10f42a5435ac806586d8ec690cf571580
Epaggelia <Holmes12345@outlook.com>
2016-07-29 10:36:30 -0400

    fixed pushing rotation into array

--- public_html/game.js
+++ public_html/game.js
@@ -121,10 +121,10 @@ function startLogic()
 		{
 			for (var y = 0; y < tileCountY; y++)
 			{
-				//ctx.rotate((90*spriteTiles[x][y].rotation)*Math.PI/180);
+				ctx.rotate((90*spriteTiles[x][y].rotation)*Math.PI/180);
 				ctx.drawImage(image,
-						//spriteTiles[x][y].id * 32, // srcX the correct version
-						spriteTiles[x][y] * 32, // srcX the temp version
+						spriteTiles[x][y].id * 32, // srcX the correct version
+						//spriteTiles[x][y] * 32, // srcX the temp version
 						0, // srcY
 						32, 32,
 						x * bgTile.w, y * bgTile.h,
@@ -171,68 +171,70 @@ function startLogic()
 	
 	function genMap()
 	{
-		var rotate = 0;
 		var EDGE = 2;		//defaults to fenceless edge
 		var CORNER = 3;		//defaults to fenceless corner
 				
-		if (Math.random() > 0.5)//random fence
-		{
-			EDGE = 4;
-			CORNER = 5;
-		}
+//		if (Math.random() > 0.5)//random fence
+//		{
+//			EDGE = 4;
+//			CORNER = 5;
+//		}
 		
 		for ( var x = 0; x < tileCountX; x++ )
 		{
-			spriteTiles [x] = [];
+			var id = 0;
+			var rotation = 0;
+			
+			spriteTiles[x]=[];
 			for ( var y = 0; y < tileCountY; y++ )
 			{
-				spriteTiles[x][y] = 0;
+				id = 0;
 				
 				//level border in clockwise order from top left
 				if (y == 0)								// top side
-					spriteTiles[x][y] = EDGE;
+					id = EDGE;
 				if (x == tileCountX-1)					// right side
 				{
-					spriteTiles[x][y] = EDGE;
-					rotate = 1;
+					id = EDGE;
+					rotation = 1;
 				}
 				if (y == tileCountY-1)					// bottom side
 				{
-					spriteTiles[x][y] = EDGE;
-					rotate = 2;
+					id = EDGE;
+					rotation = 2;
 				}	
 				if (x == 0)								// left side
 				{
-					spriteTiles[x][y] = EDGE;
-					rotate = 3;
+					id = EDGE;
+					rotation = 3;
 				}
 				
 				//level corner --- draws over border
 				if (x == 0 && y == 0)						//NW
-					spriteTiles[x][y] = CORNER;
+					id = CORNER;
 				if (x == tileCountX-1 && y == 0)			//NE
 				{
-					spriteTiles[x][y] = CORNER;
-					rotate = 1;
+					id = CORNER;
+					rotation = 1;
 				}
 				if (x == tileCountX-1 && y == tileCountY-1)	//SE
 				{
-					spriteTiles[x][y] = CORNER;
-					rotate = 2;
+					id = CORNER;
+					rotation = 2;
 				}
 				if (x == 0 && y == tileCountY-1)			//SW
 				{
-					spriteTiles[x][y] = CORNER;
-					rotate = 3;
+					id = CORNER;
+					rotation = 3;
 				}
 				
 				//random rocks 1 space inside border
 				if (x > 1 && x < (tileCountX-1)-1)
 					if (y > 1 && y < (tileCountY-1)-1)
 						if (Math.random() > 0.95)
-						spriteTiles[x][y] = 1;
+						id = 1;
 			
-				
+				spriteTiles[x].push(new spriteID(id,rotation));
 			}
 		}
 	}
