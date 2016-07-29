a2f579aabb859d3875367315ce4b7bc003782526
jordan-desktop <sandworm55@hotmail.com>
2016-07-28 22:59:34 -0400

    fixed braces and formatting

--- public_html/game.js
+++ public_html/game.js
@@ -45,7 +45,7 @@ function startLogic()
 	var gameState = MENU;
 
 	var sprites = [ ];
-	var spriteTiles = [[]];
+	var spriteTiles = [ [ ] ];
 
 	var background = new SpriteObject();
 	background.srcY = 192;
@@ -126,87 +126,90 @@ function startLogic()
 
 	function playGame()
 	{
-		
+
 	}
 
 	function endGame()
 	{
 
 	}
-	
+
 	var spriteID = function ( id, rotation )
- 	{
- 		if ( id < 0 || id > 5 )
- 			console.log( "Invalid ID : " + id );
- 		if ( rotation < 0 || rotation > 3 )
- 			console.log( "Invalid Rotation : " + rotation );
- 
- 		this.id = id;
- 		this.rotation = rotation;
- 	};
- 
- 	// dirt = 0
- 	// rock = 1
- 	// grass edge = 2
- 	// grass corner = 3
- 	// fence edge = 4
- 	// fence corner = 5
- 	// 
- 	// rotation is 0-3
- 	// 90*rotation clockwise
-	
+	{
+		if ( id < 0 || id > 5 )
+			console.log( "Invalid ID : " + id );
+		if ( rotation < 0 || rotation > 3 )
+			console.log( "Invalid Rotation : " + rotation );
+
+		this.id = id;
+		this.rotation = rotation;
+	};
+
+	// dirt = 0
+	// rock = 1
+	// grass edge = 2
+	// grass corner = 3
+	// fence edge = 4
+	// fence corner = 5
+	// 
+	// rotation is 0-3
+	// 90*rotation clockwise
+
 	function genMap()
 	{
 		var spriteNum = 0;
 		var EDGE = 2;		//defaults to fenceless edge
 		var CORNER = 3;		//defaults to fenceless corner
-		
-		if (Math.random() > 0.5) //random fence
+
+		if ( Math.random() > 0.5 ) //random fence
+		{
 			EDGE = 4;
 			CORNER = 5;
-		
+		}
+
 		for ( var x = 0; x < tileCountX; x++ )
 		{
-			spriteTiles [x] = [];
+			spriteTiles [x] = [ ];
 			for ( var y = 0; y < tileCountY; y++ )
 			{
 				spriteTiles[x][y] = 0;
-				
+
 				//level border in clockwise order from top left
-				if (y == 0)								// top side
+				if ( y == 0 )								// top side
 					spriteTiles[x][y] = EDGE;
-				if (x == tileCountX-1)					// right side
+				if ( x == tileCountX - 1 )					// right side
 					spriteTiles[x][y] = EDGE;
-				if (y == tileCountY-1)					// bottom side
+				if ( y == tileCountY - 1 )					// bottom side
 					spriteTiles[x][y] = EDGE;
-				if (x == 0)								// left side
+				if ( x == 0 )								// left side
 					spriteTiles[x][y] = EDGE;
-				
+
 				//level corner --- draws over border
-				if (x == 0 && y == 0)						//NW
+				if ( x == 0 && y == 0 )						//NW
 					spriteTiles[x][y] = CORNER;
-				if (x == tileCountX-1 && y == 0)			//NE
+				if ( x == tileCountX - 1 && y == 0 )			//NE
 					spriteTiles[x][y] = CORNER;
-				if (x == tileCountX-1 && y == tileCountY-1)	//SE
+				if ( x == tileCountX - 1 && y == tileCountY - 1 )	//SE
 					spriteTiles[x][y] = CORNER;
-				if (x == 0 && y == tileCountY-1)			//SW
+				if ( x == 0 && y == tileCountY - 1 )			//SW
 					spriteTiles[x][y] = CORNER;
-				
+
 				//random rocks 1 space inside border
-				if (x > 1 && x < (tileCountX-1)-1)
-					if (y > 1 && y < (tileCountY-1)-1)
-						if (Math.random() > 0.95)
-						spriteTiles[x][y] = 1;
+				if ( x > 1 && x < ( tileCountX - 1 ) - 1 )
+					if ( y > 1 && y < ( tileCountY - 1 ) - 1 )
+						if ( Math.random() > 0.95 )
+							spriteTiles[x][y] = 1;
 			}
 		}
 	}
-	
-	function redraw() {
-		for (var x = 0; x < tileCountX; x++)
+
+	function redraw()
+	{
+		for ( var x = 0; x < tileCountX; x++ )
 		{
-			for (var y = 0; y < tileCountY; y++)
+			for ( var y = 0; y < tileCountY; y++ )
 			{
-				switch (spriteTiles[x][y])
+				switch ( spriteTiles[x][y] )
 				{
 					case 5: //fenceCorner
 						spriteNum = 5;
@@ -228,16 +231,16 @@ function startLogic()
 						break;
 				}
 
-				ctx.drawImage(image,
-						Math.floor(spriteNum) * 32, // srcX
+				ctx.drawImage( image,
+						Math.floor( spriteNum ) * 32, // srcX
 						0, // srcY
 						32, 32,
 						x * bgTile.w, y * bgTile.h,
-						32, 32);
+						32, 32 );
 			}
 		}
 	}
-	
+
 	genMap();
 	update();
 }
