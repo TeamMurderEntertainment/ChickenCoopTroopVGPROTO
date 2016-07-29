ca3fb5e5698efdaf0d9876b9e938a6b20933efb0
Epaggelia <Holmes12345@outlook.com>
2016-07-28 23:31:55 -0400

    setup level gen and prepared for rotating

--- public_html/game.js
+++ public_html/game.js
@@ -157,13 +157,15 @@ function startLogic()
 	
 	function genMap()
 	{
-		var spriteNum = 0;
+		var rotate = 0;
 		var EDGE = 2;		//defaults to fenceless edge
 		var CORNER = 3;		//defaults to fenceless corner
-		
-		if (Math.random() > 0.5) //random fence
+				
+		if (Math.random() > 0.5)//random fence
+		{
 			EDGE = 4;
 			CORNER = 5;
+		}
 		
 		for ( var x = 0; x < tileCountX; x++ )
 		{
@@ -176,27 +178,47 @@ function startLogic()
 				if (y == 0)								// top side
 					spriteTiles[x][y] = EDGE;
 				if (x == tileCountX-1)					// right side
+				{
 					spriteTiles[x][y] = EDGE;
+					rotate = 1;
+				}
 				if (y == tileCountY-1)					// bottom side
+				{
 					spriteTiles[x][y] = EDGE;
+					rotate = 2;
+				}	
 				if (x == 0)								// left side
+				{
 					spriteTiles[x][y] = EDGE;
+					rotate = 3;
+				}
 				
 				//level corner --- draws over border
 				if (x == 0 && y == 0)						//NW
 					spriteTiles[x][y] = CORNER;
 				if (x == tileCountX-1 && y == 0)			//NE
+				{
 					spriteTiles[x][y] = CORNER;
+					rotate = 1;
+				}
 				if (x == tileCountX-1 && y == tileCountY-1)	//SE
+				{
 					spriteTiles[x][y] = CORNER;
+					rotate = 2;
+				}
 				if (x == 0 && y == tileCountY-1)			//SW
+				{
 					spriteTiles[x][y] = CORNER;
+					rotate = 3;
+				}
 				
 				//random rocks 1 space inside border
 				if (x > 1 && x < (tileCountX-1)-1)
 					if (y > 1 && y < (tileCountY-1)-1)
 						if (Math.random() > 0.95)
 						spriteTiles[x][y] = 1;
+			
+				
 			}
 		}
 	}
@@ -206,30 +228,9 @@ function startLogic()
 		{
 			for (var y = 0; y < tileCountY; y++)
 			{
-				switch (spriteTiles[x][y])
-				{
-					case 5: //fenceCorner
-						spriteNum = 5;
-						break;
-					case 4: // fenceEdge
-						spriteNum = 4;
-						break;
-					case 3: // grassCorner
-						spriteNum = 3;
-						break;
-					case 2: // grassEdge
-						spriteNum = 2;
-						break;
-					case 1: // rock
-						spriteNum = 1;
-						break;
-					default: // dirt
-						spriteNum = 0;
-						break;
-				}
-
+				
 				ctx.drawImage(image,
-						Math.floor(spriteNum) * 32, // srcX
+						spriteTiles[x][y] * 32, // srcX
 						0, // srcY
 						32, 32,
 						x * bgTile.w, y * bgTile.h,
