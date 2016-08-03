b90dc9575de6256c0bbdb42a82adcd34398005bc
jordan-desktop <sandworm55@hotmail.com>
2016-08-03 16:46:28 -0400

    added more comments to basicFunctions.js

--- public_html/libraries/basicFunctions.js
+++ public_html/libraries/basicFunctions.js
@@ -25,18 +25,40 @@ function removeObject(objectToRemove,array)
 	}
 }
 
-function getPercentage(a,b)
+/**
+ * returns an string value 0-100 that is the percentage a is of b
+ * 
+ * @param {int} a number to be checked against b
+ * @param {int} b total value
+ * @param {bool} asString default true, false if you want an int value back
+ * @returns {String/int} returns 0-100 in string or int representing percent
+ */
+function getPercentage(a,b,asString)
 {
-	return Math.floor((a / b)*100).toString();
+	if (asString == null)
+		asString = true;
+	
+	if (asString)
+		return Math.floor((a / b)*100).toString();
+	else
+		return Math.floor((a / b)*100);
 }
 
-//converts from degrees to radians
+/**
+ * converts from degrees to radians
+ * @param {int} degrees number to be converted
+ * @returns {Number} converted number
+ */
 Math.radians = function(degrees) 
 {
 	return degrees * Math.PI / 180;
 };
  
-// Converts from radians to degrees.
+/**
+ * converts from radians to degrees
+ * @param {int} radians number to be converted
+ * @returns {Number} converted number
+ */
 Math.degrees = function(radians)
 {
 	return radians * 180 / Math.PI;
